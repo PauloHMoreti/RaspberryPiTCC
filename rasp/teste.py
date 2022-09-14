b6a2d1615f2527ace2b6318f001bc9902ec06bb7
Paulo Henrique Moreti Dos Santos <89671373+SourFlame8@users.noreply.github.com>
2022-09-14 20:19:07 -0300

    Add files via upload

---
+++ rasp/teste.py
@@ -0,0 +1,17 @@
+import RPi.GPIO as GPIO
+from time import sleep
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(4, GPIO.IN)
+
+try:
+    while True:
+        if GPIO.input(4):
+            print("Vazando")
+        else:
+        print("Safe")
+
+        sleep(1)
+
+finally:
+    print("Termina")
+    GPIO.cleanup()
