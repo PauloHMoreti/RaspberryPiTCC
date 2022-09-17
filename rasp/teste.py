cb33b449177468997394e23781b1915205f42a2e
Paulo Henrique Moreti Dos Santos <89671373+SourFlame8@users.noreply.github.com>
2022-09-16 22:26:51 -0300

    Update teste.py

--- rasp/teste.py
+++ rasp/teste.py
@@ -2,16 +2,18 @@ import RPi.GPIO as GPIO
 from time import sleep
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(4, GPIO.IN)
+GPIO.setup(17, GPIO.IN)
 
 try:
     while True:
         if GPIO.input(4):
             print("Vazando")
-            print(GPIO.value(4))
+            print(GPIO.input(4))
+            print(GPIO.input(17))
         else:
             print("Safe")
-            print(GPIO.value(4))
-
+            print(GPIO.input(4))
+            print(GPIO.input(17))
         sleep(1)
 
 finally:
