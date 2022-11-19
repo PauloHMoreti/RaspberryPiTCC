efdc26a0725b824910c7c8a76e8f3fd630fe3b50
Paulo Henrique Moreti Dos Santos <89671373+SourFlame8@users.noreply.github.com>
2022-11-19 11:03:46 -0300

    Device Finalizado

--- rasp/Funcional/Funcional.py
+++ rasp/Funcional/Funcional.py
@@ -2,34 +2,44 @@
 import RPi.GPIO as GPIO
 from time import sleep
 import requests
-from getmac import get_mac_address as deviceNumber
+from getmac import get_mac_address as MacAddress
 
 #Configurações Gerais:
 portaSensor = 4
 vazamento = 0
+deviceNumber = 19
+host = "http://10.1.9.1:38000/device/dashboard"
 
 #Configurações do GPIO:
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(portaSensor, GPIO.IN)
-sensor = GPIO.input(portaSensor)
+
+#Iniciando o sistema:
+firstPost = requests.post(host + f"/{deviceNumber}/0")
+print(firstPost)
 
 #Codigo Principal:
 try:
     while True:
-        if sensor:
+        if GPIO.input(portaSensor):
             print("Sem Vazamento")
-            print(sensor)
+            print(GPIO.input(portaSensor))
         else:
             print("Tá Vazando")
-            print(sensor)
+            print(GPIO.input(portaSensor))
             vazamento += 1
         
         sleep(1)
 
         if vazamento > 4:
             print("Enviando dados para o servidor")
+            vazamentoPost1 = requests.post(host + f"/{deviceNumber}/1")
+            print(vazamentoPost1)
             sleep(10)
-
+            vazamento = 0
+            vazamentoPost2 = requests.post(host + f"/{deviceNumber}/0")
+            print(vazamentoPost2)
+            
 finally:
     print("Termina")
     GPIO.cleanup()
--- rasp/testeRequests.py
+++ rasp/testeRequests.py
@@ -1,11 +1,18 @@
 import requests
 from getmac import get_mac_address as gma
 
-a = requests.get("https://gsense.azurewebsites.net/GSense-0.0.1-SNAPSHOT/device/test/1/1")
+#a = requests.get("https://gsense.azurewebsites.net/GSense-0.0.1-SNAPSHOT/device/test/1/1")
+a = requests.post("http://10.1.9.1:38000/device/dashboard/2003/0")
 print(gma())
 print(a)
 
 
 #idDevice: 1
 #deviceNumber: 1
-#Vazamento: 
+#Vazamento:
+
+device = 1
+host = "http://10.1.9.1:38000/device/dashboard"
+print(host + f"/{device}")
+
+
